76c82762063d6e93d2cfe31180f08cfa25b8fa5b
haytham5 <78439856+haytham5@users.noreply.github.com>
2023-10-06 20:02:59 -0400

    Add operator and ostream to Order.h

--- Orders/Order.h
+++ Orders/Order.h
@@ -15,6 +15,18 @@ public:
     virtual void execute() = 0; // Execute the order action.
     virtual ~Order() {} // Virtual destructor to ensure proper cleanup for derived classes.
     
+    Order& operator = (const Order& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const Order &object) {
+        out << object.description << "; Executed: " << object.isExecuted << endl;
+        return out;
+    }
+
     std::string getDescription() const;
     bool hasExecuted() const;
 };
@@ -32,6 +44,11 @@ public:
     void addOrder(Order* order);
     void removeOrder(Order* order);
     void moveOrder(int fromIndex, int toIndex);
+
+    OrdersList& operator = (const OrdersList& other) {
+        orders = other.orders;
+        return *this;
+    }
     
     // Getters
     const std::vector<Order*>& getOrders() const;
@@ -51,6 +68,23 @@ public:
     DeployOrder();
     DeployOrder(Territory* target, int armies);
 
+    DeployOrder& operator = (const DeployOrder& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        targetTerritory = other.targetTerritory;
+        numberOfArmies = other.numberOfArmies;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const DeployOrder &object) {
+        out << object.description << "; Executed: " << 
+        object.isExecuted << "; Target Territory: " << 
+        object.targetTerritory << "; Number of Armies: " <<
+        object.numberOfArmies << endl;
+        return out;
+    }
+
     // Overridden functions from the base Order class
     bool validate() override;
     void execute() override;
@@ -65,6 +99,26 @@ private:
 public:
     AdvanceOrder();
     AdvanceOrder(Territory* source, Territory* target, int armies);
+
+    AdvanceOrder& operator = (const AdvanceOrder& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        targetTerritory = other.targetTerritory;
+        sourceTerritory = other.sourceTerritory;
+        numberOfArmies = other.numberOfArmies;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const AdvanceOrder &object) {
+        out << object.description << "; Executed: " << 
+        object.isExecuted << "; Target Territory: " << 
+        object.targetTerritory << "; Source Territory : " << 
+        object.sourceTerritory << "; Number of Armies: " <<
+        object.numberOfArmies << endl;
+        return out;
+    }
+
     bool validate() override;
     void execute() override;
 };
@@ -78,6 +132,26 @@ private:
 public:
     AirliftOrder();
     AirliftOrder(Territory* source, Territory* target, int armies);
+
+    AirliftOrder& operator = (const AirliftOrder& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        targetTerritory = other.targetTerritory;
+        sourceTerritory = other.sourceTerritory;
+        numberOfArmies = other.numberOfArmies;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const AirliftOrder &object) {
+        out << object.description << "; Executed: " << 
+        object.isExecuted << "; Target Territory: " << 
+        object.targetTerritory << "; Source Territory : " << 
+        object.sourceTerritory << "; Number of Armies: " <<
+        object.numberOfArmies << endl;
+        return out;
+    }
+
     bool validate() override;
     void execute() override;
 };
@@ -89,6 +163,23 @@ private:
 public:
     BlockadeOrder();
     BlockadeOrder(Territory* target);
+
+    BlockadeOrder& operator = (const BlockadeOrder& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        targetTerritory = other.targetTerritory;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const BlockadeOrder &object) {
+        out << object.description << "; Executed: " << 
+        object.isExecuted << "; Target Territory: " << 
+        object.targetTerritory << "; Source Territory : " << endl;
+        return out;
+    }
+    
+
     bool validate() override;
     void execute() override;
 };
@@ -100,6 +191,22 @@ private:
 public:
     BombOrder();
     BombOrder(Territory* target);
+
+    BombOrder& operator = (const BombOrder& other) {
+        description = other.description;
+        isExecuted = other.isExecuted;
+        targetTerritory = other.targetTerritory;
+        return *this;
+    }
+
+    //Ostream
+    friend ostream & operator << (ostream &out, const BombOrder &object) {
+        out << object.description << "; Executed: " << 
+        object.isExecuted << "; Target Territory: " << 
+        object.targetTerritory << "; Source Territory : " << endl;
+        return out;
+    }
+
     bool validate() override;
     void execute() override;
 };
